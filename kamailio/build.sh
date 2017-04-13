aa5f9eaa91b96c185323b94fd5cb9ad91ce47ed6
Luciano Rezende <lrezende@daitangroup.com>
2017-04-13 16:20:03 -0300

    Change config branch to ours

--- kamailio/build.sh
+++ kamailio/build.sh
@@ -2,13 +2,14 @@
 NETWORK=${NETWORK:-"kazoo"}
 REPO=${2:-"https://github.com/kamailio/kamailio.git"}
 KAMAILIO_BRANCH=4.4
-CONFIG_BRANCH=4.0
+CONFIG_BRANCH=alz-4.0
 
 if [ ! -d "etc/kazoo-configs" ]; then
        git clone https://github.com/alianza-dev/kazoo-configs.git ./etc/kazoo-configs
 fi
 
 cd etc/kazoo-configs
+git fetch
 git checkout $CONFIG_BRANCH
 git reset --hard $CONFIG_BRANCH
 cd ../../
