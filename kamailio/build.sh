ad4dd7a6d1f60d31598c86f54dcffbfb0638e59e
Luciano Rezende <lrezende@daitangroup.com>
2017-04-13 13:26:13 -0300

    Checkout the config outside the container; change default config version.

--- kamailio/build.sh
+++ kamailio/build.sh
@@ -2,8 +2,18 @@
 NETWORK=${NETWORK:-"kazoo"}
 REPO=${2:-"https://github.com/kamailio/kamailio.git"}
 COMMIT=4.4.5
+CONFIG=4.0.0
 
 echo $COMMIT > etc/commit
+
+if [ -d "etc/kazoo-configs" ]; then
+	cd etc/kazoo-configs
+	git checkout $CONFIG
+	git reset --hard $CONFIG
+	cd ../../
+else
+	git clone -b $CONFIG https://github.com/alianza-dev/kazoo-configs.git etc/kazoo-configs
+fi
 
 docker build $BUILD_FLAGS \
 	--build-arg REPO=$REPO \
--- kamailio/build/setup-config.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -e
-CONFIG=${CONFIG:-"master"}
-git clone -b 4.0 https://github.com/alianza-dev/kazoo-configs.git kazoo-configs
-cd kazoo-configs
-git reset --hard $CONFIG
-cd ../
