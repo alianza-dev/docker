672e864b7f001fd8a7cbf9a3d5254175b75f0b75
Paulo Vicentini <pvicentini@daitangroup.com>
2017-04-13 09:39:45 -0300

    remove pre configured settings

--- kazoo/build/run.sh
+++ kazoo/build/run.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 COMMAND=${1:-"console"}
 export KAZOO_CONFIG=$HOME/config.ini
-sed -i "s|couchdb.kazoo|$COUCHDB|" $KAZOO_CONFIG
-sed -i "s|rabbitmq.kazoo|$RABBITMQ|" $KAZOO_CONFIG
 export KAZOO_NODE=$NODE_NAME@$(hostname)
 export KAZOO_APPS=sysconf,blackhole,callflow,cdr,conference,crossbar,fax,hangups,media_mgr,milliwatt,omnipresence,pivot,registrar,reorder,stepswitch,teletype,trunkstore,webhooks,ecallmgr
 export RELX_REPLACE_OS_VARS=true
